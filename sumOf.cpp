a6866a22fe1fc9cdb576c1b33158486ac2cea0b0
mkiser71 <mkiser71@gmail.com>
2021-05-21 07:49:13 -0400

    Add files via upload

---
+++ sumOf.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace::std;
+
+int main()
+{
+	cout << "\n\n Print the sum of two numbers :\n";
+	cout << "-----------------------------------\n";
+	int a;
+	int b;
+	int sum;
+	a=29;
+	b=30;
+	sum=a+b;
+	cout << "The sum of " << a << " and " << b << " is: " << sum <<  "\n\n";
+}
+		
