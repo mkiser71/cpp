182e91a92de3bd0c6f5d9a8c0e58648e809a0ef6
mkiser71 <mkiser71@gmail.com>
2021-05-21 07:49:49 -0400

    Add files via upload

---
+++ inOut.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+
+{
+
+
+	//void hello()
+
+	{
+
+		cout << "What is your name?\n";
+
+		string str;
+
+		cin >> str;
+
+		cout << "Hello, " << str << "!\n";
+
+	}
+
+	return 0;
+
+
+
+}
---
+++ printFunction.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+ 
+void print() {
+  std::cout << "Hello World!";
+}
+ 
+int main() { 
+  print();
+}
